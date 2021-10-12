f2cd945cba38010cc1b0d68450c9cb8f32aea026
Govindu <govindu415417@gmail.com>
2021-10-12 10:52:15 +0530

    Add project files.

---
+++ CrudOperationWithWCFService/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace CrudOperationWithWCFService
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Insert = new System.Windows.Forms.Button();
+            this.lbltitle = new System.Windows.Forms.Label();
+            this.lblname = new System.Windows.Forms.Label();
+            this.lblsalary = new System.Windows.Forms.Label();
+            this.lblcity = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.lblmsg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Insert
+            // 
+            this.Insert.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Insert.Location = new System.Drawing.Point(149, 389);
+            this.Insert.Name = "Insert";
+            this.Insert.Size = new System.Drawing.Size(75, 23);
+            this.Insert.TabIndex = 0;
+            this.Insert.Text = "Insert";
+            this.Insert.UseVisualStyleBackColor = true;
+            this.Insert.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbltitle
+            // 
+            this.lbltitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lbltitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltitle.ForeColor = System.Drawing.Color.White;
+            this.lbltitle.Location = new System.Drawing.Point(2, 9);
+            this.lbltitle.Name = "lbltitle";
+            this.lbltitle.Size = new System.Drawing.Size(1500, 58);
+            this.lbltitle.TabIndex = 1;
+            this.lbltitle.Text = "User Registration Form";
+            this.lbltitle.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblname
+            // 
+            this.lblname.AutoSize = true;
+            this.lblname.Location = new System.Drawing.Point(53, 186);
+            this.lblname.Name = "lblname";
+            this.lblname.Size = new System.Drawing.Size(53, 17);
+            this.lblname.TabIndex = 2;
+            this.lblname.Text = "Name :";
+            this.lblname.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblsalary
+            // 
+            this.lblsalary.AutoSize = true;
+            this.lblsalary.Location = new System.Drawing.Point(50, 241);
+            this.lblsalary.Name = "lblsalary";
+            this.lblsalary.Size = new System.Drawing.Size(56, 17);
+            this.lblsalary.TabIndex = 3;
+            this.lblsalary.Text = "Salary :";
+            // 
+            // lblcity
+            // 
+            this.lblcity.AutoSize = true;
+            this.lblcity.Location = new System.Drawing.Point(24, 298);
+            this.lblcity.Name = "lblcity";
+            this.lblcity.Size = new System.Drawing.Size(76, 17);
+            this.lblcity.TabIndex = 4;
+            this.lblcity.Text = "CityName :";
+            this.lblcity.Click += new System.EventHandler(this.lblcity_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(125, 183);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(141, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(125, 238);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(141, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(125, 293);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(141, 22);
+            this.textBox3.TabIndex = 7;
+            // 
+            // lblmsg
+            // 
+            this.lblmsg.AutoSize = true;
+            this.lblmsg.BackColor = System.Drawing.Color.White;
+            this.lblmsg.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblmsg.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.lblmsg.Location = new System.Drawing.Point(125, 346);
+            this.lblmsg.Name = "lblmsg";
+            this.lblmsg.Size = new System.Drawing.Size(59, 20);
+            this.lblmsg.TabIndex = 9;
+            this.lblmsg.Text = "label1";
+            this.lblmsg.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblmsg);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblcity);
+            this.Controls.Add(this.lblsalary);
+            this.Controls.Add(this.lblname);
+            this.Controls.Add(this.lbltitle);
+            this.Controls.Add(this.Insert);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Insert;
+        private System.Windows.Forms.Label lbltitle;
+        private System.Windows.Forms.Label lblname;
+        private System.Windows.Forms.Label lblsalary;
+        private System.Windows.Forms.Label lblcity;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label lblmsg;
+    }
+}
+
---
+++ CrudOperationWithWCFService/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CrudOperationWithWCFService.ServiceReference1;
+
+namespace CrudOperationWithWCFService
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        ServiceReference1.Service1Client client = new ServiceReference1.Service1Client();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            InsertEmployee u = new InsertEmployee();
+            u.Name = textBox1.Text;
+            u.Salary = Convert.ToInt32(textBox2.Text);
+            u.CityName = textBox3.Text;
+            string res = client.Insert(u);
+            lblmsg.Text = res.ToString();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblcity_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
